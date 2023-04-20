965cc3c7c585ffa3b28c52389def0295bbd98e75
kiyoshi713 <112112870+kiyoshi713@users.noreply.github.com>
2023-04-19 20:00:52 -0500

    cambio

--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ import App from './login/App';
 import Perfil from './perfil/perfil.jsx';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import BusquedaPage from './BusquedaPage';
+
+import Experto from './perfil experto/experto';
+import Crear from './crear cuenta/crear';
+
 import NuevoUsuarioP from './NuevoUsuarioPage';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,7 +20,12 @@ root.render(
     <Route path= '/soft2-Trabajo/' element = {<App/>} />
     <Route path= '/soft2/' element = {<Perfil/>} />
     <Route path= '/busqueda/' element = {<BusquedaPage/>} />
+
+    <Route path= '/experto/' element = {<Experto/>} />
+    <Route path= '/crear/' element = {<Crear/>} />
+
     <Route path= '/NuevoUsuario/' element = {<NuevoUsuarioP/>} />
+
     </Routes>
     </BrowserRouter>
   </React.StrictMode>
