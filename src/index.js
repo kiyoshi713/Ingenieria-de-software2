49cc3600bf01ac65d8a8209580b67a852d77200a
darroyo2 <113885194+darroyo2@users.noreply.github.com>
2023-04-19 19:39:05 -0500

    Routeo página de búsqueda

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './login/App';
 import Perfil from './perfil/perfil.jsx';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import BusquedaPage from './BusquedaPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,6 +13,7 @@ root.render(
     <Routes>
     <Route path= '/soft2-Trabajo/' element = {<App/>} />
     <Route path= '/soft2/' element = {<Perfil/>} />
+    <Route path= '/busqueda/' element = {<BusquedaPage/>} />
     </Routes>
     </BrowserRouter>
   </React.StrictMode>
